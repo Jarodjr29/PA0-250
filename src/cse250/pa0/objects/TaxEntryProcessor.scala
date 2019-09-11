80db607e682e8b999705de826412260ba50b9e14
Jarodjr29 <47609939+Jarodjr29@users.noreply.github.com>
2019-09-11 18:42:26 -0400

    sanitizeData handles commas within cells and removes the necessary indexes. Every line contains 27 elements as is supposed to.

--- src/cse250/pa0/objects/TaxEntryProcessor.scala
+++ src/cse250/pa0/objects/TaxEntryProcessor.scala
@@ -27,75 +27,32 @@ import cse250.assignments.objects.TaxEntry
 import scala.collection.mutable.ListBuffer
 
 object TaxEntryProcessor {
-  def sanitizeData(filename: String): ListBuffer[Array[String]] = {
+  def sanitizeData(filename: String): ListBuffer[Array[String]]= {
     // For opening files, look at Scala Cookbook File I/O Excerpt
     val inputFile = scala.io.Source.fromFile(filename)
     // Note: lines is an iterator to the file. This is only valid as long as the file is open.
     //       Ensure you do not close the file prior to finishing the file usage.
     val lines = inputFile.getLines()
-
+    var count = 0
     var s = ListBuffer[String]()
     var clean_data = ListBuffer[Array[String]]()
-
     val remove_indexes = Array(1,2,8,9,10,11,12,13,14,21,22,23,25,32,33,34,40,41,42)
-
     for(line <- lines){
-      var cleaned_line = ListBuffer[String]()
-      var ln1 = line.replaceAll(""","""", ",@").replaceAll("""",""", "@,").replaceAll("""""""", "@").split(",", -1)
-      var ln2 = ListBuffer[String]()
-      for(s <- ln1){
-        if(s.contains("@")){
-          if(s == ln1.last){
-            ln2 += s.replaceAll("@", "")
-          } else{
-            if(ln1(ln1.indexOf(s) - 1).contains('@')){
-              ln2 += ln1(ln1.indexOf(s) - 1).concat(",").concat(s).replaceAll("@", "")
-            }
-          }
-        } else{ ln2 += s}
-      }
-      for(x <- 1 to ln2.length){
-        if(remove_indexes.contains(x) != true){
-          cleaned_line += ln2(x - 1)
-        }
-      }
-      //var ln2 = line.split(",",-1).toBuffer
-     /* breakable {
-        for (x <- ln2) {
-          try {
-            if(x == ln2.head){
-              cleaned_line += x
-            }else if (x == ln2.last) {
-              cleaned_line += x.replaceAll("@", "")
-            } else {
-              /*val next_x_index = ln2.indexOf(x) +1
-          val next_x = ln2(next_x_index)*/
-              if (remove_indexes.contains(ln2.indexOf(x) - 1) != true) {
-                if (x.contains("@") == true) {
-                  var new_x = x.concat(",").concat(ln2(ln2.indexOf(x) + 1)).replace("@", "")
-                  cleaned_line += new_x
-                  ln2 -= ln2(ln2.indexOf(x) + 1)
-                } else {
-                  cleaned_line += x.replaceAll("@", "")
-                }
-              }
-            }
-          } catch {
-            case ex: java.lang.NullPointerException => break()
-          }
-        }
-      }*/
+      count += 1
+      var cleaned_line = parseLine(line, remove_indexes)
       clean_data += cleaned_line.toArray
     }
     val outputFile = new BufferedWriter(new FileWriter( new File(filename + "-updated")))
-
+    for(data <- clean_data){
+      var line = data.mkString(",")
+      outputFile.write(line + "\n")
+    }
     // Without the '\n' character, all output will be written as one long line.
     // Process the lines.
-
-
     // Close the files at the end.
     inputFile.close()
     outputFile.close()
+    print(clean_data.length)
     clean_data
   }
 
@@ -105,5 +62,30 @@ object TaxEntryProcessor {
 
   def computeOldestEntry(filename: String): TaxEntry = {
     new TaxEntry
+  }
+
+  def parseLine(line: String, remove_indexes: Array[Int]): ListBuffer[String] = {
+    var ln1 = line.replaceAll(""","""", ",@").replaceAll("""",""", "@,").replaceAll("""""""", "@").split(",", -1)
+    var cleaned_line = ListBuffer[String]()
+    var ln2 = ListBuffer[String]()
+    for(s <- ln1){
+      if(s.contains("@")){
+        if(s.contains("(")){
+          ln2 += s.concat(", ").concat(ln1.last).replace("@", "")
+        } else{
+          if(ln1(ln1.indexOf(s) - 1).contains('@')){
+            ln2 += ln1(ln1.indexOf(s) - 1).concat(",").concat(s).replaceAll("@", "")
+          }
+        }
+      } else if(s.contains(")")){
+        null
+      } else{ ln2 += s}
+    }
+    for(x <- 1 to ln2.length){
+      if(remove_indexes.contains(x) != true){
+        cleaned_line += ln2(x - 1)
+      }
+    }
+    cleaned_line
   }
 }
